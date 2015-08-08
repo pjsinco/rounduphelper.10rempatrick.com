77ebd9b989d487a0b3214f153cd8fb81de27c049
pjsinco <psinco@fas.harvard.edu>
2015-08-08 13:31:45 -0500

    Scaffold js file; set up gulp

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    console.log('hiya');
+
+});
+
+//# sourceMappingURL=app.js.map
---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    console.log('hiya');
+
+
+});
+
+//# sourceMappingURL=app.js.map
